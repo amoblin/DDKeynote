6957324ef083f1a06700fe12ecd4bc72d1f7c955
amoblin@mbp <amoblin@gmail.com>
2015-12-14 06:28:24 +0800

    Initial Commit

---
+++ DDKeynote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDKeynote
+//
+//  Created by amoblin on 15/12/14.
+//  Copyright © 2015年 marboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDKeynote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DDKeynote
+//
+//  Created by amoblin on 15/12/14.
+//  Copyright © 2015年 marboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
